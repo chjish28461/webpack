9298733fcb6a17101de4b62578349689a8cf4e21
Crist <chjish28461@163.com>
2022-05-23 10:32:23 +0800

    feat: 将打包后的js注入到boduy底部

--- config/webpack.commom.js
+++ config/webpack.commom.js
@@ -1,6 +1,8 @@
 
 const path = require('path');
 const HtmlPlugin = require("html-webpack-plugin")
+//清除输出目录，免得每次手动删除
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const resolve = (dir)=>{
     return path.resolve(__dirname,dir)
@@ -65,10 +67,12 @@ const configs = {
         port: 8080
     },
     plugins:[
+        new CleanWebpackPlugin(),
         new HtmlPlugin(
             {
                 template:'./src/index.html',     //-------->这个文件必须指明路径
-                filename:'index.html',       //将a.html打包到有打包的js文件那个目录下的b.html(并且制动引入打包的js文件)       
+                filename:'index.html',       //将a.html打包到有打包的js文件那个目录下的b.html(并且制动引入打包的js文件) 
+                inject:'body', // 将打包后的js注入到body底部      
                 minify:{
                     collapseWhitespace:false,    //移除空格
                     removeComment:true        //删除注释
